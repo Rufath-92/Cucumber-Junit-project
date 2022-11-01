ea6620455d8f0cd6eb5178cf6fec73cb1f007885
Rufat <huseynovrufat.usa@gmail.com>
2022-11-01 14:03:39 -0400

    Updated

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -22,14 +22,14 @@ public class Driver {
     We make it static, because we want it to run before anyting else,
     also we will use it in static method
      */
-    private static WebDriver driver;
+    private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
 
     /*
     Create re-usable utility method which will return same driver instance when we call it.
      */
     public static WebDriver getDriver(){
 
-        if(driver == null){  // if driver/browser was never opened
+        if(driverPool.get() == null){  // if driver/browser was never opened
 
             String browserType = ConfigReader.getProperty("browser");
 
@@ -40,42 +40,119 @@ public class Driver {
             switch(browserType){
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    driverPool.set(new ChromeDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    driverPool.set(new FirefoxDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
                 case "safari":
                     WebDriverManager.safaridriver().setup();
-                    driver = new SafariDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+                    driverPool.set(new SafariDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
                     break;
                 case "EDGE":
                     WebDriverManager.edgedriver().setup();
-                    driver = new EdgeDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+                    driverPool.set(new EdgeDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
                     break;
 
             }
         }
         // Same driver instance will be returned every time we call Driver.getDriver() method
-        return driver;
+        return driverPool.get();
     }
 
     /*
     This method will make sure our driver value is always null after using quit() method
      */
     public static void closeDriver(){
-        if(driver != null){
-            driver.quit();  // this line will terminate the existing session. Value will not ever be null
-            driver = null;
+        if(driverPool != null){
+            driverPool.get().quit();  // this line will terminate the existing session. Value will not ever be null
+            driverPool.remove();
         }
     }
+
+
+
+
+
+//     OLD Version
+//    /*
+//     Creating a private constructor, so we are closing access to the object of this class
+//     from outside of any classes
+//     */
+//    private Driver(){}
+//
+//    /*
+//    Making our 'driver' instance private, so that it is not reachable from outside of any class
+//    We make it static, because we want it to run before anyting else,
+//    also we will use it in static method
+//     */
+//    private static WebDriver driver;
+//
+//    /*
+//    Create re-usable utility method which will return same driver instance when we call it.
+//     */
+//    public static WebDriver getDriver(){
+//
+//        if(driver == null){  // if driver/browser was never opened
+//
+//            String browserType = ConfigReader.getProperty("browser");
+//
+//        /*
+//        Depending on the browserType our switch statement will determine
+//        to open specific type of browser/driver
+//         */
+//            switch(browserType){
+//                case "chrome":
+//                    WebDriverManager.chromedriver().setup();
+//                    driver = new ChromeDriver();
+//                    driver.manage().window().maximize();
+//                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//                    break;
+//                case "firefox":
+//                    WebDriverManager.firefoxdriver().setup();
+//                    driver = new FirefoxDriver();
+//                    driver.manage().window().maximize();
+//                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//                    break;
+//                case "safari":
+//                    WebDriverManager.safaridriver().setup();
+//                    driver = new SafariDriver();
+//                    driver.manage().window().maximize();
+//                    driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+//                    break;
+//                case "EDGE":
+//                    WebDriverManager.edgedriver().setup();
+//                    driver = new EdgeDriver();
+//                    driver.manage().window().maximize();
+//                    driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+//                    break;
+//
+//            }
+//        }
+//        // Same driver instance will be returned every time we call Driver.getDriver() method
+//        return driver;
+//    }
+//
+//    /*
+//    This method will make sure our driver value is always null after using quit() method
+//     */
+//    public static void closeDriver(){
+//        if(driver != null){
+//            driver.quit();  // this line will terminate the existing session. Value will not ever be null
+//            driver = null;
+//        }
+//    }
+
+
+
+
 }
